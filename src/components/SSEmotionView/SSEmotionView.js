60e67530816913b7835661c0036caa6f3a1349a0
Jeremie Poisson <jeremie@weboxigen.com>
2017-01-22 07:28:11 -0500

    test

--- src/components/SSEmotionView/SSEmotionView.js
+++ src/components/SSEmotionView/SSEmotionView.js
@@ -3,10 +3,24 @@ import template from './ss.emotion_view.html';
 SSEmotionViewController.$inject = [];
 function SSEmotionViewController() {
     let ctrl = this;
+    let computedPositivity = 0.0;
 
     ctrl.$onInit = function () {
         ctrl.data = ctrl.data || [];
     };
+
+    /*ctrl.$onChanges = function(changes) {
+        console.log(changes);
+        if(changes.data != undefined) {
+            computedPositivity = changes.data.currentValue.reduce((acc, c) => (c.emotion == "joy" ? acc + 1 : acc), 0);
+            console.log(computedPositivity);
+        }
+    };*/
+
+    ctrl.computePositivity = function() {
+        let computedPositivity = ctrl.data.reduce((acc, c) => (c.emotion == "joy" ? acc + 1 : acc), 0);
+        return computedPositivity / ctrl.data.length;
+    }
 }
 
 const SSEmotionView = {
