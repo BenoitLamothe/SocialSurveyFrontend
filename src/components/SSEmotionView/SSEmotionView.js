ac09dc5c2dd92e27fca658b0dccdb9ef3a7fc3c8
Jeremie Poisson <jeremie@weboxigen.com>
2017-01-22 07:29:23 -0500

    removed positivity

--- src/components/SSEmotionView/SSEmotionView.js
+++ src/components/SSEmotionView/SSEmotionView.js
@@ -8,19 +8,6 @@ function SSEmotionViewController() {
     ctrl.$onInit = function () {
         ctrl.data = ctrl.data || [];
     };
-
-    /*ctrl.$onChanges = function(changes) {
-        console.log(changes);
-        if(changes.data != undefined) {
-            computedPositivity = changes.data.currentValue.reduce((acc, c) => (c.emotion == "joy" ? acc + 1 : acc), 0);
-            console.log(computedPositivity);
-        }
-    };*/
-
-    ctrl.computePositivity = function() {
-        let computedPositivity = ctrl.data.reduce((acc, c) => (c.emotion == "joy" ? acc + 1 : acc), 0);
-        return computedPositivity / ctrl.data.length;
-    }
 }
 
 const SSEmotionView = {
